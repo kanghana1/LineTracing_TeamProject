fe5980658c88d83ab4a9f11c2988712a4ed3c164
kanghana1 <qazplm12093@naver.com>
2023-11-19 22:04:31 +0900

    feedback to 최우석

--- LineTracing_TeamProject.c
+++ LineTracing_TeamProject.c
@@ -14,6 +14,7 @@ int speed = 15;
 int x = 0;
 int y = 0;
 int start_val = 0;
+int bound;
 int colorMapping[5][5];
 
 
@@ -170,90 +171,68 @@ void moveRobot() {
 
 
 int calcBound(){ // boundary value
-   int bound;
-   int yellow = 0;
-   int white = 0;
-   int r,g,b;
+      int bound;
+      int yellow = 0;
+      int white = 0;
+      int r,g,b;
 
-   for(int i =0;i<20;i++){
-      getColorRGB(cs,r,g,b);
+      for(int i = 0 ; i < 20 ; i++) {
+            getColorRGB(cs,r,g,b);
    		yellow += b; // yellow and white have the big gap between value b
-   		sleep(10);
-	 }
-   setMotorSpeed(lm,0);
-   setMotorSpeed(rm,40);
-   sleep(300);
-   setMotorSpeed(lm,0);
-   setMotorSpeed(rm,0);
+            sleep(10);
+	}
+      setMotorSpeed(lm,0);
+      setMotorSpeed(rm,40);
+      sleep(300);
+      setMotorSpeed(lm,0);
+      setMotorSpeed(rm,0);
 
-   for(int i =0;i<20;i++){
-      getColorRGB(cs,r,g,b);
+      for(int i =0;i<20;i++){
+            getColorRGB(cs,r,g,b);
    		white += b ;   // yellow and white have the big gap between value b
-   		sleep(10);
-		}
+            sleep(10);
+	}
 
-		bound = (white / 20 + yellow / 20) / 2;
-
-		return bound;
+	bound = (white / 20 + yellow / 20) / 2;
+	return bound;
 }
 
-// #define YELLOW_COLOR 1
-// #define RED_COLOR   2
-// #define BLUE_COLOR  3
-// #define WHITE_COLOR 4
-
 /*
+이거 위 calcBound에서 구한 bound값을 이용해 트레이싱 하는 알고리즘으로 바꾼 후,
+moveRobot 함수 안에 넣어야 하지 않을까요??
+
 void cross(){
 
 		while(1) {
-		   int left = getColorLeft();
-		   int right = getColorRight();
-		   int cnt_blue = 0;
-		   int cnt_red = 0;
-		   int cnt_none = 0;
-		   int front = getColorFront();
+		      int left = getColorLeft();
+		      int right = getColorRight();
+		      int cnt_blue = 0;
+		      int cnt_red = 0;
+		      int cnt_none = 0;
+		      int front = getColorFront();
 
    			if ((left == 1 && right ==4)||(left == 4 && right ==1)){
-      		go(10);
-      		sleep(500);
-      		if (front == 2){
-        				cnt_blue++;
-     			}
-     			else if (front == 3){
-        				cnt_red++;
-     			}
-      		else {
-        				cnt_none++;
-     			}
-     			go(10);
-     			sleep(500);
-   			}
-		}
+      		      go(10);
+      		      sleep(500);
+      		      if (front == 2){
+        			      cnt_red++;
+     			      }
+     			      else if (front == 3){
+        			      cnt_blue++;
+     			      }
+      		      else {
+        			      cnt_none++;
+     			      }
+     			      go(10);
+     			      sleep(500);
+   		}
+	}
 } */
 
 
 
 task main() {
-   int gray = calcBound();
-   int r,g,b;
-
-
-   while(1)
-   {
-      getColorRGB(cs,r,g,b);
-
-      if (b >= gray)
-      {
-         setMotorSpeed(lm,40);
-         setMotorSpeed(rm,20);
-   	}
-   	else
-   	{
-      setMotorSpeed(lm,40);
-      setMotorSpeed(rm,20);
-   	}
-   	cross();
-   	printPatch();
+      moveRobot();
+      printPatch();
 }
 
-}
