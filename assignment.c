8274f4a38c46aadfbdafcb33cee67b8325508979
rud15dns@naver.com <113186906+rud15dns@users.noreply.github.com>
2023-11-20 19:56:08 +0900

    지금까지짠 내용 설명

--- assignment.c
+++ assignment.c
@@ -1,13 +1,32 @@
 #define MAX_POINTS 100 // max points
 #define MAX_DISTANCE 1000000000 // max distance
 #define INF 987654321
+
+/*explanation function & variables
+1. point = the location of point(x, y)
+2. calculate = calcualte the distance of point1 and point2. return distance.
+			 = it will help comparing the distance of red patches and current location. 
+3. findshortestDistance = to find the shortest distance among redponits. 
+						its parameters are 1) current location x, y 2) red patches location x, y 3) the number of red patches.
+4. first current poisition will be (4, 4)
+5. isPointzero is to check whether robot is on (0, 0) or not. 
+6. bell_ford = to find weights between verteces and edges
+7. redpoint[max_point] is the location of red patches. it will help to compare distance between red patches and current position
+*/
+
+// not reversed, same order array for now. 
+
+/*operate
+1. When robot go to first red patch, drop the that red patch in redpoint[], and redcount--;*/
+
+
 typedef struct Point {
 	int x;
 	int y;
 } Point;
 
 
-float Calculate(Point point1, Point point2){
+float Calculate(Point point1, Point point2){ //calculate the distance of point1 and point2. 
 	return abs((point1.x + point1.y) - (point2.x + point2.y));
 }
 
@@ -25,7 +44,7 @@ float findShortestDistance(Point currentPosition, Point redPoint, int redPointCo
 }
 
 
-Point currentPosition = {0, 0};
+Point currentPosition = {4, 4};
 bool isPointZero(Point point){
 	return (point.x == 0 && point.y == 0);
 }
